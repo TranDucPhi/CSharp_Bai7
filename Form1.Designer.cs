489ffbef85f9df84d1c57a203e11bffdf7271609
ADMIN <ADMIN@LAB1-MAY33>
2023-09-11 10:54:57 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace CSharp_Bai7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSoA = new System.Windows.Forms.TextBox();
+            this.txtSoB = new System.Windows.Forms.TextBox();
+            this.txtSoC = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(468, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GIẢI PHƯƠNG TRÌNH BẬC 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập Số A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(490, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nhập Số C";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(256, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 22);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Nhập Số B";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 22);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "KẾT QUẢ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(62, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 22);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Nghiệm";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtSoA
+            // 
+            this.txtSoA.Location = new System.Drawing.Point(126, 92);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(130, 20);
+            this.txtSoA.TabIndex = 2;
+            // 
+            // txtSoB
+            // 
+            this.txtSoB.Location = new System.Drawing.Point(354, 92);
+            this.txtSoB.Name = "txtSoB";
+            this.txtSoB.Size = new System.Drawing.Size(130, 20);
+            this.txtSoB.TabIndex = 2;
+            // 
+            // txtSoC
+            // 
+            this.txtSoC.Location = new System.Drawing.Point(594, 92);
+            this.txtSoC.Name = "txtSoC";
+            this.txtSoC.Size = new System.Drawing.Size(130, 20);
+            this.txtSoC.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(148, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 22);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "X2";
+            this.label7.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(148, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 22);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "X1";
+            this.label8.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(168, 208);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(260, 20);
+            this.txtKQ.TabIndex = 2;
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(198, 269);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(319, 20);
+            this.txtX1.TabIndex = 2;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(198, 302);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(319, 20);
+            this.txtX2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(342, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Tính";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(63, 154);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 22);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Delta";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(164, 154);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 22);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSoC);
+            this.Controls.Add(this.txtSoB);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSoA;
+        private System.Windows.Forms.TextBox txtSoB;
+        private System.Windows.Forms.TextBox txtSoC;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Bai7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int intA, intB, intC;
+            double x1, x2, delta;
+            intA = Convert.ToInt32(txtSoA.Text);
+            intB = Convert.ToInt32(txtSoB.Text);
+            intC = Convert.ToInt32(txtSoC.Text);
+            if (intA == 0) { 
+                if (intB == 0) {
+                    if (intC == 0) {
+                        txtKQ.Text = "Phương Trình Có Vô Số Nghiệm";
+                    } else {
+                        txtKQ.Text = "Phương Trình Vô Nghiệm";
+                    }
+                } else {
+                    x1 = (double)-intC / intB;
+                    txtKQ.Text = "Phương Trình Có Nghiệm Duy Nhất";
+                    txtX1.Text += "" + x1;
+                }
+
+            } else { 
+                delta = Math.Pow(intB, 2) - 4 * intA * intC;
+                label10.Text = delta.ToString();
+                if (delta < 0)
+                {
+                    txtKQ.Text = "Phương Trình Có Vô Số Nghiệm";
+                }
+                else if (delta == 0)
+                {
+                    x1 = x2 = -intB / (2 * intA);
+                    txtKQ.Text = "Phương Trình Vô Nghiệm";
+                }
+                else
+                {
+                    x1 = (-intB + Math.Sqrt(delta)) / (2 * intA);
+                    x2 = (-intB - Math.Sqrt(delta)) / (2 * intA);
+                    txtKQ.Text = "Phương Trình Có Hai Nghiệm Phân Biệt";
+                    txtX1.Text += "" + (x1);
+                    txtX2.Text += "" + (x2); 
+                }
+            }
+        }
+    }
+}
